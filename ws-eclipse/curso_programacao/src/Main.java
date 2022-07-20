46a632489f3b50e40fd49470248c061f338cdf83
Gustavo Keller <gustavo.lealtec@gmail.com>
2022-07-20 13:03:19 -0300

    Variaveis e fixacao em Java

--- ws-eclipse/curso_programacao/src/Main.java
+++ ws-eclipse/curso_programacao/src/Main.java
@@ -1,10 +1,32 @@
+import java.util.Locale;
 
 public class Main {
 
 	public static void main(String[] args) {
-
-		System.out.println("Olá Mundo!");
-
+		
+		int y = 32;
+		double x = 10.35784;
+		
+		System.out.println(y);
+		System.out.println(x);
+		System.out.printf("%.2f%n", x);
+		System.out.printf("%.4f%n", x);
+		
+		System.out.print("Olá Mundo!"); // Sem quebra de linha
+		System.out.println("Bom dia!"); // Com quebra de linha
+		
+		Locale.setDefault(Locale.US);
+		System.out.printf("%.4f%n", x);
+		
+		System.out.println("Resultado = " + x + " Metros");
+		
+		System.out.printf("Resultado = %.2f metros%n", x);
+		
+		String nome = "Maria";
+		int idade = 31;
+		double renda = 4000.0;
+		System.out.printf("%s tem %d anos e ganha %.2f reias%n", nome, idade, renda);
+		
 	}
 
 }
---
+++ ws-eclipse/curso_programacao/src/fixacao.java
@@ -0,0 +1,21 @@
+
+public class fixacao {
+
+	public static void main(String[] args) {
+		String product1 = "Computer";
+		String product2 = "Office desk";
+		
+		int age = 30;
+		int code = 5290;
+		char gender = 'F';
+		
+		double price1 = 2100.0;
+		double price2 = 650.50;
+		double measure = 53.234567;
+		
+		
+		System.out.printf("Products:%n%s, which price is $ %.2f%n%s, which price is $ %.2f", product1, price1, product2, price2);
+
+	}
+
+}
