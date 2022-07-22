0daeb44ea8783562f3d8424e2ec110c0afe9dbab
Gustavo Keller <gustavo.lealtec@gmail.com>
2022-07-22 13:24:28 -0300

    Update Main Java

--- ws-eclipse/curso_programacao/src/Main.java
+++ ws-eclipse/curso_programacao/src/Main.java
@@ -1,31 +1,32 @@
-import java.util.Locale;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		int y = 32;
-		double x = 10.35784;
+		double x = 3.0;
+		double y = 4.0;
+		double z = -5.0;
+		double A, B, C;
 		
-		System.out.println(y);
-		System.out.println(x);
-		System.out.printf("%.2f%n", x);
-		System.out.printf("%.4f%n", x);
+		A = Math.sqrt(x);
+		B = Math.sqrt(y);
+		C = Math.sqrt(25);
+		System.out.println("Raiz quadrada de " + x + " = " + A);
+		System.out.println("Raiz quadrada de " + y + " = " + B);
+		System.out.println("Raiz quadrada de 25 = " + C);
 		
-		System.out.print("Olá Mundo!"); // Sem quebra de linha
-		System.out.println("Bom dia!"); // Com quebra de linha
+		A = Math.pow(x, y);
+		B = Math.pow(x, 2.0);
+		C = Math.pow(5.0, 2.0);
+		System.out.println(x + " elevado a " + y + " = " + A);
+		System.out.println(x + " elevado ao quadrado = " + B);
+		System.out.println("5 elevado ao quadradro = " + C);
 		
-		Locale.setDefault(Locale.US);
-		System.out.printf("%.4f%n", x);
-		
-		System.out.println("Resultado = " + x + " Metros");
-		
-		System.out.printf("Resultado = %.2f metros%n", x);
-		
-		String nome = "Maria";
-		int idade = 31;
-		double renda = 4000.0;
-		System.out.printf("%s tem %d anos e ganha %.2f reias%n", nome, idade, renda);
+		A = Math.abs(y);
+		B = Math.abs(z);
+		System.out.println("Valor absoluto de " + y + " = " + A);
+		System.out.println("Valor absoluto de " + y + " = " + A);
 		
 	}
 
